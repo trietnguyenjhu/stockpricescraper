1f37decb433b96c92fc7546710e403ee1e5f77ce
Triet Nguyen <trietnguyenjhu@gmail.com>
2019-09-09 22:07:39 +0000

    refactored proxy error handling

--- updateprices.py
+++ updateprices.py
@@ -19,26 +19,50 @@ def run(database, tickers, logger):
     count = 0
 
     for ticker in tickers:
-        flagIterTicker = True
-
         # console log
         now = datetime.datetime.now()
         print(f'{now.month:02.0f}/{now.day:02.0f}/{now.year:02.0f} ' +
               f'{now.hour:02.0f}:{now.minute:02.0f}:{now.second:02.0f} - ' +
               f'Downloading {ticker.strip().upper()} - {count}/{len(tickers)}')
 
-        proxy = {'https': random.choice(proxyPool)}
+        flagIterTicker = True
+        iterTickerCount = 0
 
         while flagIterTicker:
+            proxy = {'https': random.choice(proxyPool)}
             try:
-                data = yf.download(ticker, progress=False, proxy=proxy).reset_index()
-                flagIterTicker = False # don't redownload if original download was successful
-            except (requests.exceptions.SSLError, requests.exceptions.ProxyError) as e: # invalid proxy
-                raise exceptions.ProxyError(f'Unable to use proxy - {proxy}')
+                update(database, proxy, ticker)
+                flagIterTicker = False  # break out of while loop if download success
+            except exceptions.ProxyError as e:
                 logger.logError(e)
-                fileio.append('proxyerror.log', proxy)
-                continue # back to the beginning of the while loop
+                iterTickerCount += 1
+            time.sleep(random.random()*1) # iter delay
 
+        count += 1
+        if count%8 == 0: # batch delay
+            time.sleep(random.random()*20)
+            proxyPool = getProxyPool() # refresh proxy pool
+
+
+def getProxyPool():
+    """Generate a list of https proxies from https://www.sslproxies.org/"""
+    # proxy handling
+    page = requests.get('https://www.sslproxies.org/')
+    soup = BeautifulSoup(page.text, 'lxml')
+    proxyTable = pd.read_html(str(soup.find_all('table', {'id': 'proxylisttable'})))[0]
+    proxyPool = []
+    for row in (proxyTable[proxyTable['Https']=='yes'].iterrows()):
+        proxyPool.append(f"https://{row[1]['IP Address']}:{int(row[1]['Port'])}")
+    return proxyPool
+
+
+def update(database, proxy, ticker):
+    """Update data"""
+        try:
+            data = yf.download(ticker, progress=False, proxy=proxy).reset_index()
+        except (requests.exceptions.SSLError, requests.exceptions.ProxyError) as e: # invalid proxy
+            raise exceptions.ProxyError(f'Unable to use proxy - {proxy}')
+        else:
             selectQuery = \
                 f"""
                     select company_id
@@ -65,21 +89,3 @@ def run(database, tickers, logger):
             database.commit()
 
             database.runSQL('exec sp_deleteDuplicatePrices', verify=True)
-            time.sleep(random.random()*1)
-            count += 1
-
-            if count%8 == 0: # batch delay
-                time.sleep(random.random()*20)
-                proxyPool = getProxyPool() # refresh proxy pool
-
-
-def getProxyPool():
-    """Generate a list of https proxies from https://www.sslproxies.org/"""
-    # proxy handling
-    page = requests.get('https://www.sslproxies.org/')
-    soup = BeautifulSoup(page.text, 'lxml')
-    proxyTable = pd.read_html(str(soup.find_all('table', {'id': 'proxylisttable'})))[0]
-    proxyPool = []
-    for row in (proxyTable[proxyTable['Https']=='yes'].iterrows()):
-        proxyPool.append(f"https://{row[1]['IP Address']}:{int(row[1]['Port'])}")
-    return proxyPool
