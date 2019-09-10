efd2501606519b8b07c123da8b65acc49d3f79f7
Triet Nguyen <trietnguyenjhu@gmail.com>
2019-09-10 21:27:42 +0000

    implemented bulk download

--- updateprices.py
+++ updateprices.py
@@ -21,39 +21,38 @@ def run(database, tickers, logger):
     count = 0
 
     random.shuffle(tickers)
-    for ticker in tickers:
-        # console log
-        now = datetime.datetime.now()
-        print(f'{now.month:02.0f}/{now.day:02.0f}/{now.year:02.0f} ' +
-              f'{now.hour:02.0f}:{now.minute:02.0f}:{now.second:02.0f} - ' +
-              f'Downloading {ticker.strip().upper()} - {count}/{len(tickers)}')
 
-        flagIterTicker = True
-        iterTickerCount = 0
+    tickers = ' '.join(tickers)
 
-        while flagIterTicker:
+    # for ticker in tickers:
+    # console log
+    
 
-            # renew proxy if all popped from last iter
-            if len(proxyPool) == 0: proxyPool = getProxyPool()
-            proxyPath = random.choice(proxyPool)
-            proxy = {'https': proxyPath}
+    flagIterTicker = True
+    iterTickerCount = 0
 
-            try:
-                update(database, proxy, ticker)
-            except exceptions.ProxyError as e:
-                proxyPool.pop(proxyPool.index(proxyPath)) # remove faulty proxy from pool
-                logger.logError(e)
-                iterTickerCount += 1 # 10 retries
-                if iterTickerCount >=10: flagIterTicker = False
-            else:
-                flagIterTicker = False  # break out of while loop if download success
+    while flagIterTicker:
 
-            time.sleep(random.random()*1) # iter delay
+        # renew proxy if all popped from last iter
+        if len(proxyPool) == 0: proxyPool = getProxyPool()
+        proxyPath = random.choice(proxyPool)
+        proxy = {'https': proxyPath}
 
-        count += 1
-        if count%8 == 0: # batch delay
-            time.sleep(random.random()*20)
-            proxyPool = getProxyPool() # refresh proxy pool
+        try:
+            update(database, proxy, tickers)
+        except exceptions.ProxyError as e:
+            proxyPool.pop(proxyPool.index(proxyPath)) # remove faulty proxy from pool
+            logger.logError(e)
+            iterTickerCount += 1 # 10 retries
+            if iterTickerCount >=10: flagIterTicker = False
+        else:
+            flagIterTicker = False  # break out of while loop if download success
+
+    #     time.sleep(random.random()*1) # iter delay
+
+    # if count%8 == 0: # batch delay
+    #     time.sleep(random.random()*20)
+    #     proxyPool = getProxyPool() # refresh proxy pool
 
 
 def getProxyPool():
@@ -70,10 +69,10 @@ def getProxyPool():
     return proxyPool
 
 
-def update(database, proxy, ticker):
+def update(database, proxy, tickers):
     """Update data"""
     try:
-        data = yf.download(ticker, progress=False, proxy=proxy).reset_index()
+        bulkData = yf.download(tickers, progress=True, proxy=proxy)
     except (
         requests.exceptions.SSLError,
         requests.exceptions.ProxyError,
@@ -83,45 +82,57 @@ def update(database, proxy, ticker):
         ) as e: # invalid proxy
         raise exceptions.ProxyError(f'Unable to use proxy - {proxy}')
     else:
-        selectQuery = \
-            f"""
-                select company_id
-                from insiderTrading.Company
-                where ticker = '{ticker.upper()}'
-            """
-        insertQuery = \
-            """ 
-            """ # boiler plate, will not insert if ticker isn't in database
-        company_id = database.queryId(selectQuery, insertQuery)
+        bulkData = bulkData.swaplevel(axis=1)
+        postDownloadTickers = np.unique([x[0] for x in bulkData.columns])
+        
+        count = 0
+        for ticker in postDownloadTickers:
 
-        data['company_id'] = company_id
+            now = datetime.datetime.now()
+            print(f'{now.month:02.0f}/{now.day:02.0f}/{now.year:02.0f} ' +
+                    f'{now.hour:02.0f}:{now.minute:02.0f}:{now.second:02.0f} - ' +
+                    f'Updating {ticker.strip().upper()} - {count}/{len(postDownloadTickers)}')
 
-        baseInsert = \
-            f"""
-                insert into {globalconsts.SCHEMA}Price
-                (company_id, day, openPrice, highPrice, lowPrice, closingPrice, adjClose, volume)
-                values (?, ?, ?, ?, ?, ?, ?, ?)
-            """
+            data = bulkData[ticker]
+            selectQuery = \
+                f"""
+                    select company_id
+                    from insiderTrading.Company
+                    where ticker = '{ticker.upper()}'
+                """
+            insertQuery = \
+                """ 
+                """ # boiler plate, will not insert if ticker isn't in database
+            company_id = database.queryId(selectQuery, insertQuery)
+            data['company_id'] = company_id
 
-        data = nanhandler.removeRows(data)        
-        data.Open = data.Open.map(lambda x: float(x))
-        data.High = data.High.map(lambda x: float(x))
-        data.Low = data.Low.map(lambda x: float(x))
-        data.Close = data.Close.map(lambda x: float(x))
-        data['Adj Close'] = data['Adj Close'].map(lambda x: float(x))
-        data.Volume = data.Volume.map(lambda x: int(x))
+            baseInsert = \
+                f"""
+                    insert into {globalconsts.SCHEMA}Price
+                    (company_id, day, openPrice, highPrice, lowPrice, closingPrice, adjClose, volume)
+                    values (?, ?, ?, ?, ?, ?, ?, ?)
+                """
 
-        # processing
-        data = data[[
-            'company_id', 'Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]
+            data = nanhandler.removeRows(data)        
+            data.Open = data.Open.map(lambda x: float(x))
+            data.High = data.High.map(lambda x: float(x))
+            data.Low = data.Low.map(lambda x: float(x))
+            data.Close = data.Close.map(lambda x: float(x))
+            data['Adj Close'] = data['Adj Close'].map(lambda x: float(x))
+            data.Volume = data.Volume.map(lambda x: int(x))
 
-        params = list(data.itertuples(False, None))
-        try:
-            database.cursor.executemany(baseInsert, params)
-            database.commit()
-        except pyodbc.ProgrammingError as e:
-            print('Faulty params:')
-            print(params)
-            raise e
+            # processing
+            data = data[[
+                'company_id', 'Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]
 
-        database.runSQL('exec sp_deleteDuplicatePrices', verify=True)
+            params = list(data.itertuples(False, None))
+            try:
+                database.cursor.executemany(baseInsert, params)
+                database.commit()
+            except pyodbc.ProgrammingError as e:
+                print('Faulty params:')
+                print(params)
+                raise e
+
+            database.runSQL('exec sp_deleteDuplicatePrices', verify=True)
+            count += 1
