4efe4e9024dc81386ff632a8f860d0fa53637cb6
Triet Nguyen <trietnguyenjhu@gmail.com>
2019-09-10 16:58:23 +0000

    filtered proxies for https only

--- updateprices.py
+++ updateprices.py
@@ -63,7 +63,9 @@ def getProxyPool():
     soup = BeautifulSoup(page.text, 'lxml')
     proxyTable = pd.read_html(str(soup.find_all('table', {'id': 'proxylisttable'})))[0]
     proxyPool = []
-    for row in (proxyTable[proxyTable['Https']=='yes'].iterrows()):
+    for row in (proxyTable[
+        (proxyTable['Https']=='yes') & ((proxyTable['Anonymity']=='elite proxy') | (proxyTable['Anonymity']=='anonymous'))
+        ].iterrows()):
         proxyPool.append(f"https://{row[1]['IP Address']}:{int(row[1]['Port'])}")
     return proxyPool
 
